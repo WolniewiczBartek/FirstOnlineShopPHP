19c1c26a0f866c14387d7b0083d4dcd084ba7138
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-18 09:39:51 +0100

    Update styl.css

--- scripts/delete_uzytkownicy.php
+++ scripts/delete_uzytkownicy.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $conn = new mysqli('localhost', 'root', '', 'passwd_hash');
-$sql = "DELETE FROM uzytkownicy WHERE id=$_GET[id]";
+$sql = "UPDATE uzytkownicy set stan_id=4 WHERE id=$_GET[id]";
 $res = $conn->query($sql);
 $conn->close();
 header("location: ../admin/uzytkownicy.php");
