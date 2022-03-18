e49f7030b14925cc73b23b421e29d362dbcfd984
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-18 09:40:05 +0100

    Update weryfikacja_form.php

--- scripts/weryfikacja_form.php
+++ scripts/weryfikacja_form.php
@@ -1,11 +1,13 @@
 <?php
-$mail = $_GET['mail'];
+$mail = isset($_GET['mail']) ? $_GET['mail'] : "";
+$wyslany = isset($_GET['mail']) ? "<div>Podaj kod weryfikacji wysłany na<br>$mail</div>" : "";
 echo <<< OPOLE1
 <div id=verify>
   <h2>Weryfikacja</h2>
+  $wyslany
   <form action=../scripts/weryfikacja_uzytkownik.php method=post>
     <input type=hidden name=mail value=$mail><br>
-    <input type=text name=kod placeholder='Podaj kod weryfikacji'><br>
+    <input type=text name=kod placeholder='Podaj kod weryfikacji' required><br>
 OPOLE1;
 if(isset($_GET['info'])){
   echo "<br><span id=error>$_GET[info]</span><br>";
