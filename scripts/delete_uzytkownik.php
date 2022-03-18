ca826fec14d815b7f37f6113706599883859f721
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-18 09:39:59 +0100

    Update delete_uzytkownik.php

--- scripts/delete_uzytkownik.php
+++ scripts/delete_uzytkownik.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 $conn = new mysqli('localhost', 'root', '', 'passwd_hash');
-$sql = "delete from uzytkownicy where id=$_GET[id]";
+$sql = "UPDATE uzytkownicy set stan_id=4 where id=$_GET[id]";
 $res = $conn->query($sql);
 $conn->close();
-header("location: ../sites/userinfo.php");
+header("location: ../index.php?action=out");
 exit();
 ?>
