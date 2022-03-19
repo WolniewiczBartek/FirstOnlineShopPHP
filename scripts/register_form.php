1d66853c108a68ad11665bf8eb4edb42aead43ff
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-19 10:58:53 +0100

    Update register_form.php

--- scripts/register_form.php
+++ scripts/register_form.php
@@ -3,9 +3,10 @@ echo <<< OPOLE
 <div id="register">
   <h2>Rejestracja</h2>
   <form action="./scripts/register.php" method="post">
-    <input type=text name=mail placeholder=Email><br>
-    <input type=password name=haslo1 placeholder=Hasło><br>
-    <input type=password name=haslo2 placeholder="Powtórz hasło"><br>
+    <input type=text name=mail placeholder=Email autofocus><br>
+    <input type=password name=haslo1 id=haslo1 class=haslo placeholder=Hasło><br>
+    <input type=password name=haslo2 id=haslo2 class=haslo placeholder="Powtórz hasło"><br>
+    <span style=display:none; id=hasloinfo></span>
     <input type=text name=imie placeholder=Imię><br>
     <input type=text name=nazw placeholder=Nazwisko><br>
     <input type=date name=urod><br>
