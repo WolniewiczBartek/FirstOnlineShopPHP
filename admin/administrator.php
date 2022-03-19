1e1b24941a2d4fed380c0133565127f3d9a50b29
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-19 10:58:36 +0100

    Update administrator.php

--- admin/administrator.php
+++ admin/administrator.php
@@ -6,6 +6,7 @@ session_start();
   <head>
     <meta charset="utf-8">
     <title>Gąbkomarzenie</title>
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
     <link rel="stylesheet" href="../styl.css">
   </head>
   <body>
