98817da9929d1d81cd57b36db549151099f04294
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-27 21:37:48 +0200

    Update login_form.php

--- scripts/login_form.php
+++ scripts/login_form.php
@@ -7,13 +7,15 @@ echo <<< OPOLE1
   <form action=./scripts/login.php method=post>
     <input type=text name=mail $login autofocus><br>
     <input type=password name=haslo placeholder=Hasło><br>
+    
 OPOLE1;
 if(isset($_GET['info'])){
   echo "<br><span id=error>$_GET[info]</span><br>";
 }
 echo <<< OPOLE1
-  <input type=submit value="Zaloguj się!">
-</form>
+    <a id=forgot href=./index.php?action=forgot>Zapomniałeś hasła?</a><br>
+    <input type=submit value="Zaloguj się!">
+  </form>
+</div>
 OPOLE1;
-echo "</div>";
 ?>
