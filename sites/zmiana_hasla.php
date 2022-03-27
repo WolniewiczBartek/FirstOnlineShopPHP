b795ddf1c587856f9ef1a2a1e897a5c336fdb027
WolniewiczBartek <wolniewiczbart8@gmail.com>
2022-03-27 21:38:14 +0200

    Create zmiana_hasla.php

---
+++ sites/zmiana_hasla.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Gąbkomarzenie</title>
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+    <link rel="stylesheet" href="../styl.css">
+  </head>
+  <body>
+    <header>
+      <a href="../index.php"><img src="../images/logo.png" id="logo" alt="logo"></a>
+      <h1>Gąbkomarzenie</h1>
+      <div id="nazwa">
+        <a class=button href=../index.php><div>Wróć</div></a>
+      </div>
+    </header>
+    <div id="content" class="sameheight">
+<?php
+   require_once("../scripts/ustaw_nowe_haslo_form.php");
+?>  
+    </div>
+    <footer>Bartosz Wolniewicz &copy; 2022</footer>
+    <script type="text/javascript" src="../skrypt.js"></script>
+  </body>
+</html>
+
